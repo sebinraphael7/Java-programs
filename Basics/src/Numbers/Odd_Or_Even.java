269daa1096b71ac6e640ac0ead2369f7a212115b
investsebin <sebinraphael7@gmail.com>
2023-10-25 09:56:54 +0530

    my second commit

---
+++ Basics/src/Numbers/Odd_Or_Even.java
@@ -0,0 +1,17 @@
+package Numbers;
+
+import java.util.Scanner;
+
+public class Odd_Or_Even {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number for checking odd or even");
+		int num=sc.nextInt();
+		if(num%2==0)
+			System.out.println(num+ " is even");
+		else 
+			System.out.println(num+ " is odd");
+		
+	}
+
+}
